74675f630a1f9c26e8ea411eefba897e6a5167c6
Nagaraj <92457711+nagarajpala2041@users.noreply.github.com>
2025-12-17 12:52:11 +0000

    Add Terraform variables for resource configuration

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "rg-devops-assignment"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "acr_name" {
+  default = "devopsacr2041"
+}
+
+variable "app_service_plan_name" {
+  default = "asp-devops"
+}
+
+variable "web_app_name" {
+  default = "health-app-2041"
+}
