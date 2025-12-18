d7201609217ff1d0775c7801b86b96db0e9122f5
Nagaraj <92457711+nagarajpala2041@users.noreply.github.com>
2025-12-18 09:06:52 +0000

    Update Azure resources configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,50 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56"  # match the lock file
+    }
+  }
+}
+
+# Configure the Azure provider
 provider "azurerm" {
   features {}
   subscription_id = "fe8fe9a0-e971-4ed5-9bf3-55a345c13a8a"
   tenant_id       = "6a8d9eec-1f9e-43dc-abb9-020784059a92"
 }
 
-#  Resource Group
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "flask-app-rg"
+  location = "East US"
 }
 
-#  Azure Container Registry
+# Container Registry
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
+  name                = "flaskappacr12345"  # must be globally unique
   resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
-#  Azure Container Instance (ACI)
+# Optional: random suffix for DNS
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+# Azure Container Instance
 resource "azurerm_container_group" "aci" {
-  name                = "health-aci"
-  location            = var.location
+  name                = "flask-app-aci"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
+  ip_address_type     = "Public"
+  dns_name_label      = "flaskapp-${random_id.suffix.hex}"
 
   container {
-    name   = "health-app"
+    name   = "flask-app"
     image  = "${azurerm_container_registry.acr.login_server}/health-app:latest"
     cpu    = "0.5"
     memory = "1.5"
@@ -38,15 +55,10 @@ resource "azurerm_container_group" "aci" {
     }
   }
 
+  # Credentials to pull from ACR
   image_registry_credential {
     server   = azurerm_container_registry.acr.login_server
     username = azurerm_container_registry.acr.admin_username
     password = azurerm_container_registry.acr.admin_password
-  }
-
-  ip_address_type = "Public"
-
-  tags = {
-    environment = "dev"
   }
 }
